86d2cdf2791151b54eba43fc7f059bf3fd4e7dd1
ccedit <ccedit@126.com>
2023-11-27 02:41:02 +0800

    init

---
+++ Texture/Texture/loadImage.cpp
@@ -0,0 +1,8 @@
+//
+//  loadImage.cpp
+//  Texture
+//
+//  Created by 文强 on 2023/11/26.
+//
+
+#include <stdio.h>
---
+++ Texture/Texture/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Texture
+//
+//  Created by 文强 on 2023/11/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
